d4ee68cc43f485f21f4daea2463f18ecf2248a16
Piotr Malatyński <contact@malatynski.dev>
2024-04-17 21:27:38 +0200

    Remake Home screen

--- web_ui/src/views/Home/Home.tsx
+++ web_ui/src/views/Home/Home.tsx
@@ -1,19 +1,14 @@
 import { PageWithNavbar } from "../../layouts/PageWIthNavbar"
 import { JobPreview } from "./components/JobsPreview"
+import { ResumePreview } from "./components/ResumePreview"
 import { Tile } from "./components/Tile"
 import { UsageChart } from "./components/UsageChart"
 
 export const Home = () => {
     return (
-        <PageWithNavbar className="">
-          <div className="grid grid-cols-4 grid-rows-2 gap-4 mb-4">
-            <UsageChart className="col-span-2 row-span-2 bg-slate-100 rounded-md p-4" />
-            <Tile title="New Resumes" value={3} icon={null} subtitle="*Last 24h" />
-            <Tile title="Open Jobs" value={2} icon={null} />
-            <Tile title="Total Resumes" value={35} icon={null} />
-            <Tile title="Open Recommendations" value={7} icon={null} />
-          </div>
+        <PageWithNavbar className="grid grid-cols-2 gap-8">
           <JobPreview />
+          <ResumePreview />
         </PageWithNavbar>
     )
 }
--- web_ui/src/views/Home/components/ResumePreview.tsx
+++ web_ui/src/views/Home/components/ResumePreview.tsx
@@ -1,17 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useJobListQuery } from "../../../queries/useJobListQuery";
-import { Job } from "../../../types/job";
+import { useResumeListQuery } from "../../../queries/useResumeListQuery";
+import { Resume } from "../../../types/resume";
 
-export const JobPreview = () => {
-  const { data } = useJobListQuery();
+export const ResumePreview = () => {
+  const { data } = useResumeListQuery();
   const navigate = useNavigate()
   return (
     <div className="bg-slate-100 rounded-md max-h-full overflow-y-hidden flex flex-col shadow">
       <div className="flex flex-row px-4 py-4 shadow">
-        <span className="font-bold text-lg">Open Jobs</span>
+        <span className="font-bold text-lg">Candidates</span>
         <button
         className="ml-auto self-center border rounded pt-1 pr-2 pb-1 pl-2 text-sm font-semibold bg-slate-700 text-slate-100"
-        onClick={()=>navigate('/jobs/new')}
+        onClick={()=>navigate('/resumes/new')}
         >Add</button>
       </div>
       <div className="width-max overflow-y-scroll max-h-full px-4 flex-1">
@@ -23,9 +23,9 @@ export const JobPreview = () => {
   )
 }
 
-export const ListRow = (p: Job) => {
+export const ListRow = (p: Resume) => {
   return (
-        <Link to={`/jobs/${p.id}/general`} className='flex border-b mt-1 items-center p-2'>
+        <Link to={`/resumes/${p.id}`} className='flex border-b mt-1 items-center p-2'>
             <div className='text-sm font-semibold'>{p.name}</div>
         </Link>
   )
